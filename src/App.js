7fabfdaa3a596ae96191e00e64da969215b67588
Edjancarlos <edcnogueira@pop-os.localdomain>
2022-04-14 00:43:40 -0300

    feat: ajustes visuais e edição na forma de exibição. - Foram feitos ajustes visuais na tabela utilizando o tailwind e ajustes na renderização da tabela quando é adicionado uma nova pessoa e quando é excluído uma pessoa.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import { collection, doc, getDocs, updateDoc } from "firebase/firestore";
 import { useEffect, useState } from "react";
-import "./App.css";
 import { db } from "./database/firebase-config";
 import { createUser, deleteUser } from "./services/index";
 
 function App() {
+  const [updateList, setUpdateList] = useState(false);
   const [users, setUsers] = useState([]);
   const [newName, setNewName] = useState("");
   const [newAge, setNewAge] = useState(0);
@@ -23,26 +23,105 @@ function App() {
     };
 
     getusers();
-  }, []);
+  }, [updateList]);
+
+  const handleSubmit = async e => {
+    e.preventDefault();
+    await createUser(newName, newAge);
+    setUpdateList(!updateList);
+    setNewName("");
+    setNewAge("");
+  };
+
+  const handleDelete = id => {
+    deleteUser(id);
+    setUpdateList(!updateList);
+  };
 
   return (
-    <div className="App">
-      <input placeholder="Name..." onChange={e => setNewName(e.target.value)} />
-      <input
-        type="number"
-        placeholder="Age..."
-        onChange={e => setNewAge(e.target.value)}
-      />
+    <div className="bg-[#038c73] m-3 p-4 max-w-md mx-auto rounded-xl shadow-md overflow-hidden md:max-w-2xl">
+      <form onSubmit={e => handleSubmit(e)}>
+        <input
+          value={newName}
+          placeholder="Name..."
+          onChange={e => setNewName(e.target.value)}
+        />
+        <input
+          value={newAge}
+          type="number"
+          placeholder="Age..."
+          onChange={e => setNewAge(e.target.value)}
+        />
 
-      <button onClick={() => createUser(newName, newAge)}> Create User</button>
-      {users.map(user => (
-        <div key={user.id}>
-          <h1>{user.name}</h1>
-          <h1>{user.age}</h1>
-          <button onClick={() => updateUser(user.id, user.age)}>Increase Age</button>
-          <button onClick={() => deleteUser(user.id)}>Delete User</button>
-        </div>
-      ))}
+        <button type="submit"> Create User</button>
+      </form>
+      <div class="mt-4 relative overflow-x-auto shadow-md sm:rounded-lg">
+        <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+          <thead
+            class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400"
+            s
+          >
+            <tr>
+              <th scope="col" class="px-6 py-3">
+                Name
+              </th>
+              <th scope="col" class="px-6 py-3">
+                Age
+              </th>
+              <th scope="col" class="px-6 py-3 text-right">
+                Options
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map(user => (
+              <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                <td
+                  scope="row"
+                  class="px-6 py-4 font-medium text-gray-900 dark:text-white whitespace-nowrap"
+                >
+                  {user.name}
+                </td>
+                <td class="px-6 py-4">{user.age}</td>
+                <td class="px-6 py-4 text-right">
+                  <button onClick={() => updateUser(user.id, user.age)}>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="h-6 w-6"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      strokeWidth={2}
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"
+                      />
+                    </svg>
+                  </button>
+                  <button onClick={() => handleDelete(user.id)}>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="h-6 w-6"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      strokeWidth={2}
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
+                      />
+                    </svg>
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,11 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}", "./src/components/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    color: {
+      primary: "#038c73",
+      secondary: "#f27405",
+      text: "#f0f2f2",
+    },
     extend: {},
   },
   plugins: [],
